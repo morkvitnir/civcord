9caf0c6df587cee0c9401e1e50b02c4fb090b300
Einar Mørkvitnir <morkvitnir@gmail.com>
2020-08-21 18:06:56 +0300

    Create a help command

---
+++ commands/help.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const fs = require("fs");
+
+// import all commands into "commands" Array
+let commands = [];
+const commandFiles = fs.readdirSync("./commands").filter(file => (file.endsWith(".js") && !file.startsWith("help")));
+
+for (const file of commandFiles) {
+    const command = require(`../commands/${file}`);
+    commands.push(command);
+}
+
+
+
+module.exports = {
+    name: "help",
+    description: "",
+    execute(message, args) {
+
+        // create an ember message
+        let messageEmbed = {
+            color: 0x0099ff,
+            title: "Доступные режимы",
+            fields: [
+
+            ]
+        };
+
+        // go through commands and push their format and description values
+        for (const value of commands) {
+            messageEmbed.fields.push({
+                name: value.format,
+                value: value.description
+            });
+        }
+
+        message.channel.send({ embed: messageEmbed });
+
+    }
+}
