910a46437bd7057c58d61fb7b21711a32d0cf74d
Einar Mørkvitnir <morkvitnir@gmail.com>
2020-11-01 17:16:07 +0300

    Create discord-config.ts

---
+++ src/discord-config.ts
@@ -0,0 +1,9 @@
+import { DiscordConfig } from "./interfaces";
+
+
+const discordConfig: DiscordConfig = {
+    prefix: "civ",
+    token: "NzQyMDQ1NDkxNjU0ODE5OTMw.XzAZtw.KOJk81eAQ1NqJPyBv0HjBBcS6Uc"
+};
+
+export default discordConfig;
