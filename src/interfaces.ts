f66cebbe4c245733b1e1253f817192e0e2087d7c
Einar Mørkvitnir <morkvitnir@gmail.com>
2020-11-01 17:16:46 +0300

    Add DiscordConfig, Leader and EmbedResponse interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,8 +1,26 @@
 // import modules
 import * as Discord from "discord.js";
 
+export interface DiscordConfig {
+    prefix: string;
+    token: string;
+}
+
 export interface Command {
     name: string;
+    format: string;
     description: string;
     execute(message: Discord.Message, args: string[]): void;
+}
+
+export interface Leader {
+    name: string;
+    avatarId: string;
+    country: string;
+}
+
+export interface EmbedResponse {
+    color: number;
+    title: string;
+    fields: { name: string; value: string; inline?: boolean; }[];
 }
